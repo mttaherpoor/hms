adf7065a2f9441248f6001eda208ed2cd77305ef
Taher <131670439+mttaherpoor@users.noreply.github.com>
2026-05-26 13:54:03 +0330

    feat: add admin interface for doctor and patient

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Doctor, Notification
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'specialization', 'qualifications', 'years_of_experience']
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['doctor', 'appointment', 'type', 'seen', 'datetime_created']
--- patient/admin.py
+++ patient/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Patient, Notification
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['user', 'full_name', 'email', 'mobile', 'gender', 'dob']
+
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['patient', 'appointment', 'type', 'seen', 'datetime_created']
