b115c80732a88bf3595ac4aee5fcbb3484aaf862
Taher <131670439+mttaherpoor@users.noreply.github.com>
2026-05-27 19:46:04 +0330

    feat: implement authentication backend

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from .models import CustomUser
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
+    list_display = ['username', 'email', 'age', 'is_staff',]
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields':('age',)}),
+    )
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super().get_fieldsets(request, obj)
+
+        extra_fields = {
+            "Personal info": ["age",],
+        }
+
+        new_fieldsets = []
+
+        for name, opts in fieldsets:
+            extra_fields = extra_fields.get(name, [])
+
+            current_fields = list(opts.get("fields", ()))
+
+            for field in extra_fields:
+                if field not in current_fields:
+                    current_fields.append(field)
+
+            opts = {
+                **opts,
+                "fields": tuple(current_fields),
+            }
+            new_fieldsets.append((name, opts))
+
+        return new_fieldsets    
--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,11 @@
-from typing import Iterable
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
 
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True,max_length=50)
-    username = models.CharField(max_length=50, blank=True, null=True)
+    username = models.CharField(max_length=50, null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
