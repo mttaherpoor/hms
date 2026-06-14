42928a43c2340024bcf7402e668759ec28ff0980
Taher <131670439+mttaherpoor@users.noreply.github.com>
2026-06-14 14:48:33 +0330

    fix: add base app namespace

--- base/urls.py
+++ base/urls.py
@@ -4,17 +4,19 @@ from . import views
 from .payments.views import stripe
 from .payments.views import paypal
 
+app_name = 'base'
+
 urlpatterns = [
     path('',views.HomePageView.as_view(),name='home'),
-    path('service/<int:pk>/',views.ServiceDetailView.as_view(),name='service_detail'),
-    path('service/<int:service_id>/book/<int:doctor_id>/',views.BookAppointmentView.as_view(),name='book_appointment'),
+    path('service/<int:pk>/',views.ServiceDetailView.as_view(),name='service-detail'),
+    path('service/<int:service_id>/book/<int:doctor_id>/',views.BookAppointmentView.as_view(),name='book-appointment'),
     path('checkout/<str:billing_id>/',views.CheckoutView.as_view(),name='checkout'),
-    path('payment_status/<str:billing_id>/',views.PaymentStatusView.as_view(),name='payment_status'),
+    path('payment_status/<str:billing_id>/',views.PaymentStatusView.as_view(),name='payment-status'),
    
     # stripe
-    path('stripe_payment/<str:billing_id>/', stripe.StripePaymentView.as_view(), name='stripe_payment'),
-    path('stripe_payment_verify/<str:billing_id>/', stripe.StripePaymentVerifyView.as_view(), name='stripe_payment_verify'),
+    path('stripe_payment/<str:billing_id>/', stripe.StripePaymentView.as_view(), name='stripe-payment'),
+    path('stripe_payment_verify/<str:billing_id>/', stripe.StripePaymentVerifyView.as_view(), name='stripe-payment-verify'),
 
     # paypal
-    path('paypal_payment_verify/<str:billing_id>/', paypal.PayPalPaymentVerifyView.as_view(), name='paypal_payment_verify'),
+    path('paypal_payment_verify/<str:billing_id>/', paypal.PayPalPaymentVerifyView.as_view(), name='paypal-payment-verify'),
 ]
