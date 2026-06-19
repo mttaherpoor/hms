67af2d1e70f081e7061377cedaad385e12d3822b
Taher <131670439+mttaherpoor@users.noreply.github.com>
2026-06-20 03:11:47 +0330

    feat(config): switch from sqlite to postgres configuration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -107,13 +107,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DJANGO_POSTGRES_DB"),
+        "USER": env("DJANGO_POSTGRES_USER"),
+        "PASSWORD": get_secret("postgres_password"),
+        "HOST": env("DJANGO_POSTGRES_HOST"),
+        "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
