44f42a00731ad15686ec4315da50fa2b2075cc39
Taher <131670439+mttaherpoor@users.noreply.github.com>
2026-06-19 17:54:01 +0330

    test(accounts): improve custom user model behavior tests

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from accounts.models import UserType
+
+User = get_user_model()
+
+class UserModelTest(TestCase):
+
+    def test_username_generated_from_email(self):
+        user = User.objects.create_user(
+            username="",
+            email="john@example.com",
+            password="pass123",
+            user_type=UserType.PATIENT,
+        )
+
+        self.assertEqual(user.username, "john")
