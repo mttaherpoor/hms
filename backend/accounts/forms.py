85fe2310ab18fd0568f0a06fb267efd89e80a139
Taher <131670439+mttaherpoor@users.noreply.github.com>
2026-06-21 20:32:08 +0330

    feat(accounts): hide ADMIN role from public user registration form

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -1,11 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
+from .constants import UserType
 from .models import CustomUser
 
-class CustomUserCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm): 
     class Meta:
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ('email', 'age', 'user_type',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['user_type'].choices = [
+            c for c in UserType.choices
+            if c[0] != UserType.ADMIN
+        ]
 
 
 class CustomUserChangeForm(UserChangeForm):
